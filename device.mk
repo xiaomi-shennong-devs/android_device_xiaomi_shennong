4f87fc6a92f3f11aef758730d89cbe247a9120c8
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-20 19:21:09 +0200

    shennong: Add support for udfps long press sensor
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1

--- device.mk
+++ device.mk
@@ -15,6 +15,13 @@ PRODUCT_PACKAGES += \
     SettingsResShennong \
     WifiResShennong
 
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.xiaomi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
